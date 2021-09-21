066e97a24da86f1c2345aa169c8d96a341f0a404
Nassim <nassim.lebdiri@polymtl.ca>
2021-09-20 22:23:47 -0400

    server

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io =  require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 const open = require("open")
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
@@ -14,14 +13,11 @@ io.on('connection', (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected")
   })
-  socket.on('room', (msg) => {
-    console.log('message: ' + msg);
+  socket.on('chat message', (msg) => {
+    console.log( msg)
+         socket.broadcast.emit("chat message",(msg));
   });
-  socket.on('room', (msg) => {
-    io.emit('room', msg);
-  });
+
 });
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-   open("http://localhost:3000")
-});
+server.listen(9200, () => {
+  console.log('listening on *:9200');
